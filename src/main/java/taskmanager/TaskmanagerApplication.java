96012d2ad60b69a8a36615a740632a4956403a20
RAHUL YADAV <rahul.yadav181999@gmail.com>
2025-09-07 13:52:28 +0530

    initial commit. first wk setup done

---
+++ src/main/java/taskmanager/TaskmanagerApplication.java
@@ -0,0 +1,15 @@
+package taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerApplication {
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(TaskmanagerApplication.class, args);
+		System.out.println(System.getenv("GOOGLE_APPLICATION_CREDENTIALS"));
+	}
+
+}
